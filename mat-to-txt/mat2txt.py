59731dad0aeb6f552f9f094f7fedc2914573e57b
xwzhong <artorius.mailbox@qq.com>
2016-07-03 21:29:35 +0800

    Update mat2txt.py

--- mat-to-txt/mat2txt.py
+++ mat-to-txt/mat2txt.py
@@ -1,7 +1,13 @@
+#coding: utf-8
+#date: 2016-07-03
+#mail: artorius.mailbox@qq.com
+#author: xinwangzhong -version 0.1
+
 import scipy.io as sio
 import numpy as np  
-mat_path = r"F:\algorithm\dataset\data.mat"
+mat_path = r"F:\dataMining\DMalgorithm\data.mat"
 data=sio.loadmat(mat_path)
+print data
 data = list(data["data"])
 x = [[data[0][idx], data[1][idx]] for idx in range(2000) ]
 y = [data[2][idx] for idx in range(2000)]
@@ -14,5 +20,5 @@ for idx in range(1000):
 	y_tmp = label
 	x.append(x_tmp)
 	y.append(y_tmp)
-print x
-print y
+# print x
+# print y
