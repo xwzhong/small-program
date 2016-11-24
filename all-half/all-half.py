bd63b99ebcef8d8433b7b1959e39e974dfa00346
xwzhong <artorius.mailbox@qq.com>
2016-11-24 11:07:52 +0800

    Create all-half.py

---
+++ all-half/all-half.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+
+def all2half(unicode_string):  
+    """全角转半角"""  
+    rstring = ""  
+    for uchar in unicode_string:  
+        inside_code=ord(uchar)  
+        if inside_code == 12288:#全角空格直接转换              
+            inside_code = 32   
+        elif (inside_code >= 65281 and inside_code <= 65374): #全角字符（除空格）根据关系转化  
+            inside_code -= 65248  
+        rstring += unichr(inside_code)  
+    return rstring  
+      
+def half2all(unicode_string):  
+    """半角转全角"""  
+    rstring = ""  
+    for uchar in ustring:  
+        inside_code=ord(uchar)  
+        if inside_code == 32:#半角空格直接转化                    
+            inside_code = 12288  
+        elif inside_code >= 32 and inside_code <= 126:#半角字符（除空格）根据关系转化  
+            inside_code += 65248
+        rstring += unichr(inside_code)  
+    return rstring  
